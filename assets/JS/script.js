95728e643d1b3b181f37d664907524b8f87c9ca5
Trystan Cortez <trysyanmc22@gmail.com>
2023-09-19 19:54:01 -0500

    Adding fetch to Yelp API

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,6 @@
 const city = "Austin";
 const url = `https://api.weather.gov/points/39.7456,-97.0892`;
 let forecast;
-const yelpApiKey = `5KJgDCNwMAcVAmCvRFAqs5QsOiizehW_nA-Njeu_XlfAenCt6ew5tosAX26fz1Seb0TvfGrTPwdJ-yq1HfMx3ap6NqcdDcU50K-TZOjGjDZN3txIaox8L64gxJsDZXYx`
-
 fetch(url).then(function (response) {
     //going to be a json string 
     //parse the json
