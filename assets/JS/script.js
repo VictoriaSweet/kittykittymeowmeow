307308622f8de77968a38ba7004607b46382fe1f
VJacobo <victoriajacobo99@gmail.com>
2023-09-21 19:35:22 -0500

    added WeatherAPI

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,7 @@
 const city = "Austin";
 const url = `https://api.weather.gov/points/39.7456,-97.0892`;
+
+
 let forecast;
 fetch(url).then(function (response) {
     //going to be a json string 
@@ -15,11 +17,24 @@ fetch(url).then(function (response) {
 
                 console.log(newData)
 console.log( newData.properties.periods[0].name)
+// properties.periods[0].name;
+// properties.periods[0].temperature;
+// properties.periods[0].shortForecast;
+console.log(newData.properties.periods[0].name)
+console.log(newData.properties.periods[0].temperature)
+console.log(newData.properties.periods[0].shortForecast)
+for (let index = 0; index < 10; index++) {
+console.log(newData.properties.periods[index].name)
+console.log(newData.properties.periods[index].temperature)
+console.log(newData.properties.periods[index].shortForecast) 
+let name = newData.properties.periods[index].name
+let temperature = newData.properties.periods[index].temperature
+let shortForecast = newData.properties.periods[index].shortForecast
 
+
+}   
             })
-
         })
-
     });
 });
 
