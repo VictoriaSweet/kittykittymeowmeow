ee8872e876518c372230f7bf3fe6e4e24e062e5d
Victoria <sweet.victoria1218@gmail.com>
2023-09-24 18:53:22 -0500

    cleared data from past searches, tidied JS files, added CSS material

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,56 +1,40 @@
-function getWeatherData (x, y) {
+function getWeatherData(x, y) {
+  const city = "Austin";
+  const latitude = x;
+  const longitude = y;
+  const url = `https://api.weather.gov/points/${latitude},${longitude}`;
 
-const city = "Austin";
-const latitude = x;
-const longitude = y;
-const url = `https://api.weather.gov/points/${latitude},${longitude}`;
-
-let forecast;
-fetch(url).then(function (response) {
-    //going to be a json string 
+  let forecast;
+  fetch(url).then(function (response) {
+    //going to be a json string
     //parse the json
 
     response.json().then(function (weatherData) {
-        console.log(weatherData.properties.forecast);
-        forecast = weatherData.properties.forecast;
+      forecast = weatherData.properties.forecast;
 
-        fetch(forecast).then(function (response) {
+      fetch(forecast).then(function (response) {
+        response.json().then(function (newData) {
+          for (let index = 0; index < 5; index++) {
+            let weatherName = newData.properties.periods[index].name;
+            let temperature = newData.properties.periods[index].temperature;
+            let shortForecast = newData.properties.periods[index].shortForecast;
 
-            response.json().then(function (newData) {
-
-                console.log(newData)
-console.log( newData.properties.periods[0].name)
-// properties.periods[0].name;
-// properties.periods[0].temperature;
-// properties.periods[0].shortForecast;
-console.log(newData.properties.periods[0].name)
-console.log(newData.properties.periods[0].temperature)
-console.log(newData.properties.periods[0].shortForecast)
-for (let index = 0; index < 5; index++) {
-// // console.log(newData.properties.periods[index].name)
-// // console.log(newData.properties.periods[index].temperature)
- console.log(newData.properties.periods[index].shortForecast) 
-let weatherName = newData.properties.periods[index].name
-let temperature = newData.properties.periods[index].temperature
-let shortForecast = newData.properties.periods[index].shortForecast
-
-const weatherResult = document.querySelector(`#weather-result-${index+1}`);
-let weatherNameEl = document.createElement('h3');
-let temperatureEl = document.createElement('p');
-let shortForecastEl = document.createElement('h4');
-weatherNameEl.textContent = weatherName;
-temperatureEl.textContent = temperature;
-shortForecastEl.textContent = shortForecast;
-console.log(weatherResult)
-weatherResult.appendChild(weatherNameEl);
-weatherResult.appendChild(temperatureEl);
-weatherResult.appendChild(shortForecastEl);
-
-}   
-            })
-        })
+            const weatherResult = document.querySelector(
+              `#weather-result-${index + 1}`
+            );
+            let weatherNameEl = document.createElement("h3");
+            let temperatureEl = document.createElement("p");
+            let shortForecastEl = document.createElement("h4");
+            weatherNameEl.textContent = weatherName;
+            temperatureEl.textContent = temperature;
+            shortForecastEl.textContent = shortForecast;
+            weatherResult.innerText = "";
+            weatherResult.appendChild(weatherNameEl);
+            weatherResult.appendChild(temperatureEl);
+            weatherResult.appendChild(shortForecastEl);
+          }
+        });
+      });
     });
-});
-
-console.log(test);
+  });
 }
--- assets/JS/yelpAPI.js
+++ assets/JS/yelpAPI.js
@@ -1,147 +1,106 @@
-//Set to API key
-const yelpApiKey = "5KJgDCNwMAcVAmCvRFAqs5QsOiizehW_nA-Njeu_XlfAenCt6ew5tosAX26fz1Seb0TvfGrTPwdJ-yq1HfMx3ap6NqcdDcU50K-TZOjGjDZN3txIaox8L64gxJsDZXYx";
-const yelpApiBaseUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses"; //https://cors-anywhere.herokuapp.com
-const test = "the weather is nice";
+const yelpApiKey =
+  "5KJgDCNwMAcVAmCvRFAqs5QsOiizehW_nA-Njeu_XlfAenCt6ew5tosAX26fz1Seb0TvfGrTPwdJ-yq1HfMx3ap6NqcdDcU50K-TZOjGjDZN3txIaox8L64gxJsDZXYx";
+const yelpApiBaseUrl =
+  "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses"; //https://cors-anywhere.herokuapp.com
 
-
-const yelpEl = document.getElementById('yelp-results');
+const yelpEl = document.getElementById("yelp-results");
 
 // Function to perform a Yelp API search for parks
 function searchForParks(city) {
-    const options = {
-        method: 'GET',
-        headers: {
-            Origin: "null",
-            accept: 'application/json',
-            Authorization: `bearer ${yelpApiKey}`
-        }
-    };
-const apiUrl = `${yelpApiBaseUrl}/search?location=${city}&term=park&sort_by=best_match&limit=20`;
+  const options = {
+    method: "GET",
+    headers: {
+      Origin: "null",
+      accept: "application/json",
+      Authorization: `bearer ${yelpApiKey}`,
+    },
+  };
+  const apiUrl = `${yelpApiBaseUrl}/search?location=${city}&term=park&sort_by=best_match&limit=20`;
 
-
-fetch(apiUrl, options)
-.then(function (response) {
-response.json().then(function (parkData) {
-console.log(parkData)
-extractData(parkData);
-})
-})
-
+  fetch(apiUrl, options).then(function (response) {
+    response.json().then(function (parkData) {
+      extractData(parkData);
+    });
+  });
 }
 
-//getting park data out of API 
+//getting park data out of API
 function extractData(parkData) {
-for (let index = 0; index < 5; index++) {
-    const parkResult = document.querySelector(`#park-result-${index+1}`);
-let name = parkData.businesses[index].name
+  for (let index = 0; index < 5; index++) {
+    const parkResult = document.querySelector(`#park-result-${index + 1}`);
+    let name = parkData.businesses[index].name;
 
-let image = parkData.businesses[index].image_url;
+    let image = parkData.businesses[index].image_url;
 
+    let lat = parkData.businesses[index].coordinates.latitude;
 
+    let long = parkData.businesses[index].coordinates.longitude;
 
-let lat = parkData.businesses[index].coordinates.latitude;
+    let address = parkData.businesses[index].location.display_address[0];
 
+    let nameEl = document.createElement("h3");
+    let addressEl = document.createElement("p");
+    let latEl = document.createElement("p");
+    let longEl = document.createElement("p");
+    let imageEl = document.createElement("img");
+    nameEl.textContent = name;
+    latEl.textContent = lat;
+    longEl.textContent = long;
+    nameEl.setAttribute("class", "park-names");
+    addressEl.setAttribute("class", "park-names");
+    imageEl.setAttribute("src", image);
+    imageEl.setAttribute("class", "images");
+    addressEl.textContent = address;
 
+    parkResult.innerText = "";
+    parkResult.appendChild(nameEl);
+    parkResult.appendChild(addressEl);
+    parkResult.appendChild(imageEl);
+    parkResult.appendChild(latEl);
+    parkResult.appendChild(longEl);
 
-let long = parkData.businesses[index].coordinates.longitude;
-// console.log(parkData.businesses[index].coordinates.longitude);
-//fix the code to display the street address and the street name with city, state and zip code**
-// let address = parkData.businesses[index].location;
-// console.log(address);
-
-let address = parkData.businesses[index].location.display_address[0];
-console.log(address);
-
-//** put all the items in a box and add 'click' event to get the weather for the day**
-let nameEl = document.createElement('h3');
-let addressEl = document.createElement('p');
-let latEl = document.createElement('p');
-let longEl = document.createElement('p');
-let imageEl = document.createElement('img');
-nameEl.textContent = name;
-latEl.textContent = lat;
-longEl.textContent = long;
-nameEl.setAttribute('class', 'park-names');
-addressEl.setAttribute('class', 'park-names');
-imageEl.setAttribute('src', image);
-imageEl.setAttribute('class', 'images');
-addressEl.textContent = address;
-//object traversal, turn the object into an array 
-parkResult.appendChild(nameEl);
-parkResult.appendChild(addressEl);
-parkResult.appendChild(imageEl);
-parkResult.appendChild(latEl);
-parkResult.appendChild(longEl);
+    parkResult.addEventListener("click", function (response) {
+      getWeatherData(lat, long);
+    });
+  }
 }
 
-}
-// Get a reference to the button element
-let submitButton = document.querySelector('.submit');
+let submitButton = document.querySelector(".submit");
 
-// Add an event listener to the button
-submitButton.addEventListener('click', function(
-    response 
-) {
-    // Your code to handle the button click goes here
-    console.log("submit button response");
-});
+submitButton.addEventListener("click", function (response) {});
 
+// let weatherButton = document.querySelector("parkResult");
 
-let weatherButton = document.querySelector('#yelp-results');
+//properties.periods[index].icon
 
-weatherButton.addEventListener('click', function(
-    response
-) {
-    console.log(response.target.children[3].innerText);
-    console.log(response.target.children[4].innerText);
-    getWeatherData(response.target.children[3].innerText, response.target.children[4].innerText);
-});
+document.getElementById("search-bar").addEventListener("change", function (e) {
+  e.preventDefault();
 
+  const city = document.getElementById("search-bar").value.trim();
 
-// Handle form submission
-//search bar
-document.getElementById('search-bar').addEventListener('change', function (e) {
-e.preventDefault();
-//**insert submit button
-// add  'click' event listener**
-const city = document.getElementById('search-bar').value.trim();
-
-
-let rainOrShineSearches = [];
-if (localStorage.getItem("rainOrShineOldSearches")) {
+  let rainOrShineSearches = [];
+  if (localStorage.getItem("rainOrShineOldSearches")) {
     rainOrShineSearches.push(localStorage.getItem("rainOrShineOldSearches"));
-    console.log(rainOrShineSearches);
     rainOrShineSearches.push(city);
-} else {
+  } else {
     rainOrShineSearches = city;
-}
+  }
 
-localStorage.setItem('rainOrShineOldSearches', rainOrShineSearches);
+  localStorage.setItem("rainOrShineOldSearches", rainOrShineSearches);
 
-if (city) {
-searchForParks(city);
-}
+  if (city) {
+    searchForParks(city);
+  }
 
+  function displaySearchHistory() {
+    let searchHistory = document.querySelector("select");
+    let select = localStorage.getItem("key");
 
-// **be able to mark previous searches as favorites **
-// and to be able to go back to them with the updated weather for the day**
-// **make icon a star but if not that ok, just have something**
+    localStorage.setItem("key", "");
 
-// display search history**
-function displaySearchHistory() {
-    //"select" is class tag 
-let searchHistory = document.querySelector("select");
-let select = localStorage.getItem("key");
-
-localStorage.setItem('key', '');
-
-//retrieve search history 
-if (localStorageSearchHistory = null) {
-// **if key is found in local storage, show found key in console
-SearchHistory = JSON.parse(localStorageSearchHistory);
-return localStorageSearchHistory.getItem("key");
-//display Austin
-}
-console.log(displaySearchHistory)
-}
+    if ((localStorageSearchHistory = null)) {
+      SearchHistory = JSON.parse(localStorageSearchHistory);
+      return localStorageSearchHistory.getItem("key");
+    }
+  }
 });
